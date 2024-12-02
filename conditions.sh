e8f645719e1784cdab27b4e6069e475278b1dc7d
prabhu2904 <uyyalaprabhukumar77@gmail.com>
2024-12-01 16:59:48 -0800

    conditions

--- conditions.sh
+++ conditions.sh
@@ -2,11 +2,11 @@
 
 NUMBER=$1
 
-if( $NUMBER -gt 100 )
+if ( $NUMBER -gt 100 )
 then
-    echo "give number $NUMBER is greater than 100"
+    echo "given number $NUMBER is greater than 100"
 
 else
-  echo "give number $NUMBER not greater than 100"
+  echo "given number $NUMBER not greater than 100"
 
 fi
