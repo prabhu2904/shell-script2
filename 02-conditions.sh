c786e15ebfcc841794df9c7704308e4816709727
prabhu2904 <uyyalaprabhukumar77@gmail.com>
2024-12-01 18:02:00 -0800

    conditions

--- 02-conditions.sh
+++ 02-conditions.sh
@@ -5,7 +5,8 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
  echo "ERROR:: please run this script with root access"
+ exit 1 #you can give other than 0
 else
- echo "Now you are a root user can access"
-fi
+ echo "Now you are a root user"
+fi # it is reverse for fi , it means end of fi
 yum install mysql -y
