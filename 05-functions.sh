59154f5380125a0a59a9dcfa96067bffbc634521
prabhu2904 <uyyalaprabhukumar77@gmail.com>
2024-12-01 19:12:05 -0800

    functions

---
+++ 05-functions.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE(){ 
+if [ $? -ne 0 ]
+then
+ echo "mysql is failed"
+ exit 1
+else
+ echo "msql installation is sucess" 
+fi    
+}
+
+if [ $ID -ne 0 ]
+then
+ echo "ERROR:: please run this script with root access"
+ exit 1 #you can give other than 0
+else
+ echo "Now you are a root user"
+fi # it is reverse for fi , it means end of fi
+
+yum install mysql -y
+
+VALIDATE
+
+yum install git -y
+
+VALIDATE
