31df311291554170d3b553a442e801151e4d989b
prabhu2904 <uyyalaprabhukumar77@gmail.com>
2024-12-05 19:52:22 -0800

    changes

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -5,7 +5,14 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+    echo -e "$2...$R failed $N"
+else 
+    echo -e "$2...$GR sucess $N"
+fi       
+}
 if [ $ID -ne 0 ]
 then
     echo -e "$R ERROR:: Please run this script with root access $N"
@@ -19,9 +26,13 @@ fi # fi means reverse of if, indicating condition end
 for package in $@
 do
 
-     yum list installed $package
+    yum list installed $package
     if [ $? -ne 0 ]
-        then
-        yum install $package -y
-        
+    then
+    yum install $package -y
+    VALIDATE $? "installation of $package"
+else
+    echo -e "$package is already installed... $Y skipped $N"
+fi
+    
 done          
